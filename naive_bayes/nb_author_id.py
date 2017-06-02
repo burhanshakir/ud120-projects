544598c153ee80c988693a1a22b4038af520d615
DESKTOP-JA6MH3M\burha <burhanshakir96@gmail.com>
2017-06-02 11:11:27 +0530

    Finding time taken to predict

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,9 +29,12 @@ clf = GaussianNB()
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print("training time:", round(time()-t0, 3), "s")
+print("training time to fit:", round(time()-t0, 3), "s")
 
+t0 = time()
 pred = clf.predict(features_test)
+print("training time to predict:", round(time()-t0, 3), "s")
+
 
 from sklearn.metrics import accuracy_score
 
