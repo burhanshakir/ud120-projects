53d4f2c54c3bbcefbabaa2e91649a28fcb83203d
DESKTOP-JA6MH3M\burha <burhanshakir96@gmail.com>
2017-06-12 23:31:42 +0530

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,11 +19,29 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+
+from sklearn import svm
+
+clf = svm.LinearSVC()
+
+t0 = time()
+
+
+
+clf.fit(features_train, labels_train)
+print("training time to fit:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("training time to predict:", round(time()-t0, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+accuracy = (accuracy_score(pred, labels_test))
+
+print(accuracy)
 
 #########################################################
 
